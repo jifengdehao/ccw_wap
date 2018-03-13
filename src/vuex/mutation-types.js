2bc201cd05870a69f4146c37c58efc17078da5f1
zhangwenlong <zhangwenlong@ccw163.com>
2018-03-13 09:45:10 +0800

    09:44 地址地图bug

--- src/vuex/mutation-types.js
+++ src/vuex/mutation-types.js
@@ -3,4 +3,4 @@ export const SET_MARKET = 'SET_MARKET'  // 保存当前菜市场数据
 export const SET_USER = 'SET_USER' // 保存登录之后的个人信息
 export const SET_SHOP_CART = 'SET_SHOPCART' // 保存购物车数据
 export const ADD_SHOP_CART = 'ADD_SHOPCART' // 添加商品到购物车
-export const SET_ADDMODRESS = 'SET_ADDMODRESS' // 添加商品到购物车
+export const SET_ADDMODRESS = 'SET_ADDMODRESS' // 新增地址位置保存
